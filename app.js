0fc10e63b739269884c4b706825feb020dd16d2d
Matias Guijarro <matias.guijarro@free.fr>
2015-03-14 18:23:16 +0100

    added app.py added all JS code fixed HTML to work with current JS

--- app.js
+++ app.js
@@ -1,17 +1,90 @@
 $(function() {
- 
+  $("#material_modal_ok").click(function() {
+    add_material($("#material_modal_name").val());
+  }); 
+
+  /*
+    TODO: add default materials
+  */
+  add_material('Air', ['C1', 'N1', 'O1', 'Ar1', 'Kr1'], [0.000124, 0.75527, 0.23178, 0.012827, 3.2e-6],0.0012048,1); 
 });
 
-function add_material(e) {
-  $("#new_material_modal").modal("show");
+function add_material(name, compounds_list, mass_list, default_density, default_thickness) {
+    if (compounds_list == undefined) { compounds_list = []; };
+    if (mass_list == undefined) { mass_list = []; };
+    if (default_density == undefined) { default_density=1;  };
+    if (default_thickness == undefined) { default_thickness=1; };
 
-  var new_material = $($("#materials").children()[0]).clone();    
-  $("#materials").append(new_material);
+    material_panels = $("#materials .panel");
+    var new_material = $(material_panels[material_panels.length-1]).clone();
+    if ($(material_panels[0]).hasClass("placeholder")) {
+      $(material_panels[0]).remove();
+      new_material.removeClass("placeholder");
+    }
+    $("#materials").append(new_material);
+    var a = new_material.find(".panel-title").find("a")
+    a.text(name);
+    var new_id = "material_"+name;
+    a.attr("href", "#"+new_id);
+    new_material.find(".panel-collapse").attr("id", new_id);
+    var material_table = new_material.find("tbody");
+    var new_row = $(material_table.children()[0]).clone()
+    material_table.empty();
+    if (compounds_list.length > 0) {
+        for (var i=0; i<compounds_list.length; i++) {
+            var inputs = new_row.find("input");
+            $(inputs[0]).val(compounds_list[i]);
+            $(inputs[1]).val(mass_list[i]);
+            material_table.append(new_row);
+            new_row = new_row.clone();
+        }
+    } else {
+        var inputs = new_row.find("input");
+        $(inputs[0]).val($(inputs[0]).attr("placeholder"));
+        $(inputs[1]).val($(inputs[0]).attr("placeholder"));
+        material_table.append(new_row);
+    }
+    var material_form = new_material.find("fieldset");
+    var inputs = material_form.find("input");
+    $(inputs[0]).val(default_density);
+    $(inputs[1]).val(default_thickness);
+
+    update_materials_in_tables();
+};
+
+function update_materials_in_tables() {
+    $("#multilayer ul").empty();
+    $("#attenuators ul").empty();
+    $("#detector ul").empty();
+
+    $("#materials .panel-title a").each(function() {
+        var material_name = $(this).html();
+        $("#multilayer ul").append($("<li><a href='#' onclick='material_selected(event)'>"+material_name+"</a></li>")); 
+        $("#multilayer button").each(function() {
+          if ($.trim($(this).text())=='placeholder') { $(this).html($(this).html().replace("placeholder", material_name)) };
+        });
+        $("#attenuators ul").append($("<li><a href='#' onclick='material_selected(event)'>"+$(this).html()+"</a></li>")); 
+        $("#attenuators button").each(function() {
+          if ($.trim($(this).text())=='placeholder') { $(this).html($(this).html().replace("placeholder", material_name)) };
+        });
+        $("#detector ul").append($("<li><a href='#' onclick='material_selected(event)'>"+$(this).html()+"</a></li>")); 
+        $("#detector button").each(function() {
+          if ($.trim($(this).text())=='placeholder') { $(this).html($(this).html().replace("placeholder", material_name)) };
+        });
+    });
+};
+
+function show_add_material(e) {
+  $("#material_modal").modal("show");
 };
 
 function material_selected(e) {
     var ref = e.target;
-    alert($(ref).html());
+    var selected_material = $(ref).html();
+    var btn = $(ref).parent().parent().prev();
+    var current_material = $.trim($(btn).text());
+    $(btn).html($(btn).html().replace(current_material, selected_material));    
+    e.preventDefault();
 };
 
 function add_row(e) {
@@ -19,4 +92,105 @@ function add_row(e) {
     var table_row = $(add_btn).parent().parent();
     var new_table_row = table_row.clone();
     table_row.parent().append(new_table_row);
+};
+
+function filter_selected(e) {
+    material_selected(e);
+};
+
+function get_materials() {
+    var materials = [];
+
+    $("#materials .panel").each(function() {
+      var material = {};
+      material.name = $($(this).find(".panel-title a")[0]).text();
+      material.compounds = [];
+      material.mass = [];
+      var i = 0;
+      $(this).find("tbody input").each(function() {
+        if (i % 2 == 0) { material.compounds.push($(this).val()); }
+        else { material.mass.push($(this).val()); }
+        i++;
+      });
+      var inputs = $(this).find("fieldset input");
+      material.density = $(inputs[0]).val();
+      material.thickness = $(inputs[1]).val();
+      materials.push(material);
+    });      
+  
+    return materials;
+}; 
+
+function get_multilayer() {
+    var multilayer = [];
+
+    $("#multilayer_table tbody tr").each(function() {
+      var layer = {};
+      var input_cols = $(this).find("input");
+      layer.name = $(input_cols[0]).val();
+      layer.density = $(input_cols[1]).val();
+      layer.thickness = $(input_cols[2]).val();
+      layer.material = $.trim($($(this).find("button")[0]).text());
+      multilayer.push(layer);
+    });
+
+    return multilayer;
+};
+
+function get_attenuators() {
+    var attenuators = [];
+
+    $("#attenuators_table tbody tr").each(function() {
+      var att = {};
+      var input_cols = $(this).find("input");
+      var btns = $(this).find("button");
+      att.type = $.trim($(btns[0]).text());
+      att.density = $(input_cols[0]).val();
+      att.thickness = $(input_cols[1]).val();
+      att.funny = $(input_cols[2]).val();
+      att.material = $.trim($(btns[1]).text());
+      attenuators.push(att);
+    });
+
+    return attenuators;
+};
+
+function get_detector() {
+    var detector = {};
+
+    $("#detector_table tbody tr").each(function() {
+      var input_cols = $(this).find("input");
+      var btns = $(this).find("button");
+      detector.material = $.trim($(btns[0]).text());
+      detector.density = $(input_cols[0]).val();
+      detector.thickness = $(input_cols[1]).val();
+      detector.area = $(input_cols[2]).val();
+      detector.distance = $(input_cols[3]).val();
+    });
+
+    return detector;
+};
+
+function submit() {
+    user_input = {
+      "materials": get_materials(),
+      "attenuators": get_attenuators(),
+      "detector": get_detector(),
+      "multilayer": get_multilayer(),
+      "incoming_angle": $("#incoming_angle").val(),
+      "outgoing_angle": $("#outgoing_angle").val()
+    };
+  
+    $.ajax({ url: "calculate",
+             data: JSON.stringify(user_input),
+             dataType: "json", 
+             type: "POST",
+             contentType:"application/json; charset=utf-8",
+             success: function(returned_data) {
+                 /*
+                     TODO: better handle result: are there errors? etc
+                 */
+                 $("#result").text(returned_data.text);
+             }
+   });
 };
---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import optparse
+import bottle
+import socket
+#import logging
+import json
+
+# patch socket module;
+# by default bottle doesn't set address as reusable
+# and there is no option to do it...
+socket.socket._bind = socket.socket.bind
+
+def my_socket_bind(self, *args, **kwargs):
+    self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    return socket.socket._bind(self, *args, **kwargs)
+socket.socket.bind = my_socket_bind
+
+@bottle.route('/')
+def main():
+    return serve_static_file("app.html")
+
+@bottle.route("/<url:path>")
+def serve_static_file(url):
+    return bottle.static_file(url, os.path.dirname(__file__))
+
+@bottle.post("/calculate")
+def do_calculation():
+    user_input = json.load(bottle.request.body)
+
+    print "received:", user_input
+
+    return { "text": "The answer is 42." }
+
+def serve_forever(port=None):
+    bottle.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    usage = "usage: \%prog [-p<port>]" 
+
+    parser = optparse.OptionParser(usage)
+    parser.add_option(
+        '-p', '--port', dest='port', type='int',
+        help='Port to listen on (default 8099)', default=8099, action='store')
+    options, args = parser.parse_args()
+
+    #logging.basicConfig()
+    #logging.getLogger().setLevel(logging.DEBUG)
+
+    serve_forever(options.port)
